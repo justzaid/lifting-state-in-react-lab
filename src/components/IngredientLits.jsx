a3b691d00cf817dc2f163adcb9a75e963f8e41d8
justzxid <zaidmk96@gmail.com>
2025-02-18 16:43:39 +0300

    Burger Stack Lab

---
+++ src/components/IngredientLits.jsx
@@ -0,0 +1,18 @@
+const IngredientList = (props) => {
+    return (
+        <>
+        <ul>
+            {props.availableIngredients.map((ingredient) => (
+                <li
+                    style={{ backgroundColor: ingredient.color }}
+                    onClick={() => props.handleAddToStack(ingredient)}
+                >
+                    {ingredient.name}
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+}
+
+export default IngredientList
